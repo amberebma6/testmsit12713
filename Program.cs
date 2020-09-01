eafdd923dfb940f522019e46205464b52769f901
amberebma6 <amberyjchen@outlook.com>
2020-09-01 22:16:13 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testmsit12713
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
